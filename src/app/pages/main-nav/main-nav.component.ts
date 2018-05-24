6e0353da9a035fc70b0d0a623d4fe24bcc4149de
Danilo Luiz Rheinheimer <danilolr@gmail.com>
2018-05-24 20:06:35 -0300

    first commit

---
+++ src/app/pages/main-nav/main-nav.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.css']
+})
+export class MainNavComponent {
+
+  isHandset$ = false;
+    
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  
+  }
